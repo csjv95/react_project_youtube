0514eee68220c349e2f75806d7235c874e71a8ca
csjv95 <csjv95@gmail.com>
2020-12-21 15:25:17 +0900

    docs : Using the useeffect function, convert the popular videoos list on YouTube to json and put it in connle.log

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './app.css';
 
 function App() {
+  const [videos,setVideos] = useState([]);
+
+  useEffect(() => {
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxWidth=25&key=AIzaSyBFtWhYKei7L8SA-Hfy_-_oqZMfuT4S1G4", requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  },[]);
+
   return <h1>Hello</h1>;
 }
 

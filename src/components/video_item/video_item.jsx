7f00559f212cb02c1f16faae392a33d1cdd1140f
csjv95 <csjv95@gmail.com>
2020-12-24 11:52:42 +0900

    make video item and styling

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import styles from './video_item.module.css';
 
 const VideoItem = (props) => (
-      <li>
-        <img src={props.video.snippet.thumbnails.medium.url} alt="video"/>
-        <div>
-          <p>{props.video.snippet.title}</p>
-          <p>{props.video.snippet.channelTitle}</p>
+      <li className={styles.container}>
+        <div className={styles.video}>
+          <img className={styles.thumbnail} src={props.video.snippet.thumbnails.medium.url} alt="video"/>
+            <div className={styles.metadata}>
+              <p className={styles.title}>{props.video.snippet.title}</p>
+              <p className={styles.channelTitle}>{props.video.snippet.channelTitle}</p>
+            </div>
         </div>
       </li>
   );

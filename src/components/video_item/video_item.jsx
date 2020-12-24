0b25218f351100fec7f4aff7959b1866fa338cc1
csjv95 <csjv95@gmail.com>
2020-12-24 10:11:45 +0900

    Show video img,title,channelTitle

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const VideoItem = (props) => (
-      <li>{props.video.snippet.title}</li>
+      <li>
+        <img src={props.video.snippet.thumbnails.medium.url} alt="video"/>
+        <div>
+          <p>{props.video.snippet.title}</p>
+          <p>{props.video.snippet.channelTitle}</p>
+        </div>
+      </li>
   );
 
 export default VideoItem;

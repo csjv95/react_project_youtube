19f449ca08897355f9b32b64cf722c184fd853e1
csjv95 <csjv95@gmail.com>
2021-01-08 18:33:23 +0900

    docs : choice video and detail

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './app';
 import Youtube from './service/youtube';
 
-const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);  // 매번 
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);  // The components should be as simple as possible and network communication should be generated only once each time, many actions occur during testing or actual operation.
 
 ReactDOM.render(
   <React.StrictMode>
-    <App youtube={youtube}/>
+    <App youtube={youtube} />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/service/youtube.js
+++ src/service/youtube.js
@@ -8,24 +8,17 @@ class Youtube {
   }
 
   async mostPopular() {
-   try {
-      const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxWidth=25&key=${this.key}`, this.getrequestOptions);
-      const result = await response.json();
-      return (result.items);
-    } catch (error) {
-      return console.log('error', error);
-    }
-  }
-
+    const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxWidth=25&key=${this.key}`, this.getrequestOptions);
+    const result = await response.json();
+    return result.items;
+  } 
+  
   async search(query) {
-    try {
-      const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`, this.getrequestOptions);
-      const result = await response.json();
-      return result.items.map(item => ({ ...item, id: item.id.videoId }));
-    } catch (error) {
-      return console.log('error', error);
-    }
+    const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`, this.getrequestOptions);
+    const result = await response.json();
+    return result.items.map(item => ({ ...item, id: item.id.videoId }));
   }
 }
+
 
 export default Youtube;
